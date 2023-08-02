fed4409545d397c016f0f6b480e9b46eb34e982d
harikuppudu <120652340+harikuppudu@users.noreply.github.com>
2023-08-02 12:17:50 +0200

    Add project files.

---
+++ Selen1C/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selen1C")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selen1C")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00268ff4-5d6f-49ff-b88b-fe5c78f6a4b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Selen1C/Tests/SeleTest12.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selen1C.Tests
+{
+    public class SeleniumTest
+
+    {
+
+        [Test]
+
+        public void firstseleniumtest()
+        {
+     
+
+           IWebDriver driver = new ChromeDriver();
+           driver.Navigate().GoToUrl("https://www.selenium.dev/");      
+           Assert.That(driver.Title, Is.EqualTo("Selenium"));
+           driver.Quit();
+
+
+        }
+    }
+}
